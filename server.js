6ba9312735e8b96a440ac4e2e62cd617b89922b9
jeff forrester <jeff.forrester@gmail.com>
2019-06-29 03:31:10 -0400

    parse meta data out of data set and routes to pass it along

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
-const express = require('express');
+const path = require('path')
+const express = require('express')
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 const db = require('./server/db.js')
 
-begin = () => {
+let keys
+let symbols
+begin = ({headerKeys, symbolList}) => {
+	keys = headerKeys
+	symbols = symbolList
+	console.log(symbolList)
 	app.listen(port, () => console.log(`Listening on port ${port}`));
 }
+
+app.get('/', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 const valid = [
 	'sym',
@@ -19,6 +30,14 @@ const valid = [
 	'ask_quantity'
 ]
 
+app.get('/meta', (req, res)=>{
+	console.log(symbols,keys)
+	res.send({
+		symbolList: symbols,
+		headerKeys: keys
+	})
+})
+
 app.get('/query', (req, res) => {
 	// filter out invalid params
 	let actual = {}
@@ -27,18 +46,19 @@ app.get('/query', (req, res) => {
 			actual[param] =  req.query[param]
 		}
 	}
+	
+	actual['sym'] = actual['sym'] || 'EUR/AUD'
 
 	let count = req.query.count || 10
 	let start = req.query.start || 0
 
 	// execute a search with the valid params that were sent
 	db.find({...actual} , (error, newDocs)=>{
-		console.log(newDocs)
 		res.send({
 			total: newDocs.length,
 			length: count,
-			data: newDocs.slice(start,count)
+			data: newDocs.slice(start,count),
+			headerKeys: keys
 		})
 	})
-  
 });
--- server/db.js
+++ server/db.js
@@ -4,18 +4,29 @@ const Datastore = require('nedb')
 
 let db = new Datastore();
 
-let data
-let csv = csvToJson()
-.fromFile(csvFilePath)
-.then((jsonObj)=>{
-    data = jsonObj
-    fill(data)
-})
+csvToJson()
+	.fromFile(csvFilePath)
+	.then((jsonObj)=>{
+	    fill(jsonObj)
+	})
 
+// TODO - this is a stupid way to get the data
 fill = (data) => {
-	db.insert(data, (newDocs)=>{
-		// console.log(db.getAllData())
-		begin()
+	let symbols = {}
+	db.insert(data, (err, newDocs)=>{
+		// walk over every row to build array of unique
+		// symbols
+		let s = newDocs.forEach((doc,i)=>{
+			if(!symbols[doc.sym]){
+				symbols[doc.sym] = true
+			}
+		})
+
+		// send along column headers and unique symbols
+		begin({
+			headerKeys: Object.keys(newDocs[0]),
+			symbolList: symbols
+		})
 	})
 }
 
